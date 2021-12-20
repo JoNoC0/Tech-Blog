08e3e830cb7995e8718d89224d69d61158cf24bb
JoNoC0 <jnguyeningco@gmail.com>
2021-12-20 15:10:55 -0600

    added async function for add-post edit-post delete-post.js

---
+++ public/javascript/signup.js
@@ -0,0 +1,31 @@
+async function commentFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const username = document.querySelector('#password-signup').value.trim();
+
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    if (username && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if (response.ok) {
+            console.log('success');
+            document.location.reload('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('#signup-form').addEventListener('submit', signupFormHandler);
